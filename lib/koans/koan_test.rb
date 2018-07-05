44170a75d91e1b17a28817fc4cf20c7a148fafd9
schadenfred <fred.schoeneman@gmail.com>
2018-07-05 16:57:44 -0700

    removed require statement from koan_test

--- lib/koans/koan_test.rb
+++ lib/koans/koan_test.rb
@@ -2,6 +2,5 @@ require 'test_helper'
 require_relative 'koans_helper'
 require_relative 'manifest'
 require_relative 'koans_runner'
-require_relative '../minitest/koans_reporter'
 
 include KoansHelper
